66a2f11578d1a699162975ce3b129dc686ab1357
Lukas Metzner <lukas@metzner.io>
2024-08-03 12:35:00 +0200

    basic cobra setup for clai-cli

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"log"
 	"os"
+	"os/exec"
 
 	"github.com/spf13/cobra"
 )
@@ -12,6 +13,17 @@ var rootCmd = &cobra.Command{
 	Short: "CLI tool to interact with the Clai System",
 	Run: func(cmd *cobra.Command, args []string) {
 		log.Println("Hello from Clai")
+		command := args[0]
+		command_args := args[1:]
+		cmdex := exec.Command(command, command_args...)
+
+		stdout, err := cmdex.Output()
+
+		if err != nil {
+			log.Fatalf("%s", err)
+		}
+
+		log.Printf("%s", string(stdout))
 	},
 }
 
